7f4e2aae82f055be7bd8f2c53ed92f5167383705
Teiki Pepin <tepepin@emi.u-bordeaux1.fr>
2018-09-17 17:41:57 +0200

    Correction titre de fenetre

--- src/shepp_logan/main.cpp
+++ src/shepp_logan/main.cpp
@@ -1,12 +1,10 @@
-#include "mainwindow.h"
+#include "widget.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.setTitle("Shepp Logan");
-    w.resize(640, 480);
+    Widget w;
     w.show();
 
     return a.exec();
