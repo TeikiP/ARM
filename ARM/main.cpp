9dee3ee7c157fd6cd36b56f8143ee0dad6f06332
Enzo Peruzzetto <enzperuzzetto@emi.u-bordeaux1.fr>
2018-09-26 15:53:22 +0200

    création dans le main du tableau de vertex et tableau de couleur retourner par la classe Vertices

--- ARM/main.cpp
+++ ARM/main.cpp
@@ -86,29 +86,24 @@ void TriangleWindow::render()
     QMatrix4x4 matrix;
     matrix.perspective(60.0f, 4.0f/3.0f, 0.1f, 100.0f);
     matrix.translate(0, 0, -2);
-    matrix.rotate(100.0f * m_frame / screen()->refreshRate(), 0, 1, 0);
+    //matrix.rotate(100.0f * m_frame / screen()->refreshRate(), 0, 1, 0);
 
     m_program->setUniformValue(m_matrixUniform, matrix);
 
-    GLfloat vertices[] = {
-        0.0f, 0.707f,
-        -0.5f, -0.5f,
-        0.5f, -0.5f
-    };
+    Vertices vertex = new Vertices();
+    vertex->readFile("../shepp_logan.pgm3d");
 
-    GLfloat colors[] = {
-        1.0f, 0.0f, 0.0f,
-        0.0f, 1.0f, 0.0f,
-        0.0f, 0.0f, 1.0f
-    };
+    GLfloat *vertices = vertex.getCoords();
+    GLfloat *colors = vertex.getColors();
 
-    glVertexAttribPointer(m_posAttr, 2, GL_FLOAT, GL_FALSE, 0, vertices);
-    glVertexAttribPointer(m_colAttr, 3, GL_FLOAT, GL_FALSE, 0, colors);
+
+    glVertexAttribPointer(m_posAttr, 3, GL_FLOAT, GL_FALSE, 0, vertices);
+    glVertexAttribPointer(m_colAttr, 1, GL_FLOAT, GL_FALSE, 0, colors);
 
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_POINT, 0, vertex.getSize());
 
     glDisableVertexAttribArray(1);
     glDisableVertexAttribArray(0);
