4c9007663c71e8397137fd3c5daed42afaf40a07
Enzo Peruzzetto <enzperuzzetto@emi.u-bordeaux1.fr>
2018-10-17 15:37:41 +0200

    ajout chargement shader

--- ARM/displayWindow.cpp
+++ ARM/displayWindow.cpp
@@ -10,24 +10,8 @@
 #include <iostream>
 using namespace std;
 
-#define PGM3D_PATH "../shepp_logan.pgm3d"
-
-
-static const char *vertexShaderSource =
-    "attribute highp vec4 posAttr;\n"
-    "attribute lowp vec4 colAttr;\n"
-    "varying lowp vec4 col;\n"
-    "uniform highp mat4 matrix;\n"
-    "void main() {\n"
-    "   col = colAttr;\n"
-    "   gl_Position = matrix * posAttr;\n"
-    "}\n";
-
-static const char *fragmentShaderSource =
-    "varying lowp vec4 col;\n"
-    "void main() {\n"
-    "   gl_FragColor = col;\n"
-    "}\n";
+#define VERTEX_SHADER_PATH "../shader/vertexShader.vert"
+#define FRAGMENT_SHADER_PATH "../shader/fragmentShader.frag"
 
 
 DisplayWindow::DisplayWindow()
@@ -45,15 +29,14 @@ DisplayWindow::DisplayWindow()
 void DisplayWindow::initialize()
 {
     m_program = new QOpenGLShaderProgram(this);
-    m_program->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSource);
-    m_program->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSource);
+    m_program->addShaderFromSourceFile(QOpenGLShader::Vertex, VERTEX_SHADER_PATH);
+    m_program->addShaderFromSourceFile(QOpenGLShader::Fragment, FRAGMENT_SHADER_PATH);
     m_program->link();
     m_posAttr = m_program->attributeLocation("posAttr");;
     m_colAttr = m_program->attributeLocation("colAttr");
     m_matrixUniform = m_program->uniformLocation("matrix");
 
-    //QString filename = QFileDialog::getOpenFileName(0, tr("Open File"), ".", tr("Files (*.pgm3d *.obj)"));
-    QString filename = PGM3D_PATH;
+    QString filename = QFileDialog::getOpenFileName(0, tr("Open File"), ".", tr("Files (*.pgm3d *.obj)"));
 
     Vertices vertex;
     vertex.readFile(filename);
